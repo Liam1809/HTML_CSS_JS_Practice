025911b2e17625b1ebdd528d78d88f9fe62bc75b
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-08-04 10:09:14 +0700

    kicking off basic JS

---
+++ Bootcamp/Javascript/JS Stalker/stalker.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+alert("my full name is " + firstName + " " + lastName);
+alert("my current age is " + age);
+console.log("my full name is " + firstName + " " + lastName);
+console.log("my current age is " + age);
