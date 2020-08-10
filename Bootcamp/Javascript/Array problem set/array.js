1cb436f3d4ca1d671593e8aa9a3e00368c5ac21a
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-08-10 12:53:52 +0700

    forEach

--- Bootcamp/Javascript/Array problem set/array.js
+++ Bootcamp/Javascript/Array problem set/array.js
@@ -50,3 +50,32 @@ var max = function(arr) {
 max([1,2,3]); // 3
 max([10,3,10,4]); // 10
 max([-5,100]); // 100
+
+// forEach
+// =========
+// VERSION 1
+// =========
+function myForEach(arr, func){
+	for (var i = 0; i < arr.length; i++) {
+		func(arr[i]);
+	}
+}
+
+var colors = ["red", "orange", "yellow", "green", "blue", "PURPLE"];
+myForEach(colors, function(color){
+	console.log(color);
+});
+
+// =========
+// VERSION 2 
+// =========
+Array.prototype.myForEach = function(func){
+  for(var i = 0; i < this.length; i++) {
+   func(this[i]);
+  }
+};
+
+var colors = ["red", "orange", "yellow", "green", "blue", "PURPLE"];
+colors.myForEach(function(color){
+	console.log(color);
+});
