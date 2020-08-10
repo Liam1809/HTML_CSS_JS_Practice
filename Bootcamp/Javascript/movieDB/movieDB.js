da2f0bdbac6404c405a08d79f40f85b9ee935706
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-08-10 17:01:35 +0700

    Objects

---
+++ Bootcamp/Javascript/movieDB/movieDB.js
@@ -0,0 +1,37 @@
+var movies = [
+{
+	title: "In Bruges",
+	rating: 4,
+	hasWatched: true,
+},
+{
+	title: "John Wick",
+	rating: 5,
+	hasWatched: true,
+},
+{
+	title: "Busan 2",
+	rating: 4.5,
+	hasWatched: false,
+},
+{
+	title: "BumBoBum",
+	rating: 3.5,
+	hasWatched: true,
+}];
+
+var buildMovie = function(movie) {
+	let result = "You have ";
+	if(movie.hasWatched){
+		result += "watched ";
+	}
+	else {
+		result += "not seen ";
+	}
+	result += "\"" + movie.title + "\"" + " - " + movie.rating + " stars";
+	console.log(result); 
+};
+
+movies.forEach(function(movie){
+	buildMovie(movie);
+});
