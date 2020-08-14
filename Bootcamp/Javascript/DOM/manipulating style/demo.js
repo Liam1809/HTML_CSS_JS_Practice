d7c39c096c617a81c60cd82bc6ce6065264019ee
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-08-14 15:03:56 +0700

    textContent and innerHTML practice

--- Bootcamp/Javascript/DOM/manipulating style/demo.js
+++ Bootcamp/Javascript/DOM/manipulating style/demo.js
@@ -7,3 +7,25 @@ window.setInterval(function(){
 	//TOGGLE A CLASS
 tag.classList.toggle("big");
 },2000);
+
+// textContent
+var orange = document.getElementsByTagName("li")[0];
+console.log(orange);
+console.log(orange.textContent);
+orange.textContent = "Orange is not my fav color";
+
+var blue = document.getElementsByTagName("li")[1];
+console.log(blue);
+console.log(blue.textContent);
+blue.textContent = "<strong>Blue</strong> is my fav color";
+
+// innerHTML
+var red = document.querySelectorAll("li")[2];
+console.log(red);
+console.log(red.innerHTML);
+red.innerHTML = "Red is not my fav color";
+
+var yellow = document.querySelectorAll("li")[3];
+console.log(yellow);
+console.log(yellow.innerHTML);
+yellow.innerHTML = "<strong><em>Yellow</em></strong> is not my fav color";
